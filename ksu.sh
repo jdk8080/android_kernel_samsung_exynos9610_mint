1fce0b768bdbf06648c8028f21a91e2f8b5489c8
jdk8080 <103557604+jdk8080@users.noreply.github.com>
2026-07-21 14:59:18 +0530

    Update ksu.sh

--- ksu.sh
+++ ksu.sh
@@ -2,7 +2,7 @@
 
 [ ! -e "KernelSU/kernel/setup.sh" ] && \
 git clone https://github.com/KernelSU-Next/KernelSU-Next KernelSU && 
-cd KernelSU && git checkout 89a0d4c53e9d5d8c6d53922f342d858c4c7f300c
+cd KernelSU && git checkout 0d6bdc6364cbfc73517dcfdf7ab23b0ba8045553
 cd ..
 
 if [[ $BUILD_SUSFS == 'true' && $BUILD_KERNEL_KSU == 'true' ]]; then
